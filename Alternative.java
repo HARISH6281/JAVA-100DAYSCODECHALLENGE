c78b6f141b45af776547945edb42dee1ee44d1ef
HARISHMONDI <92738146+HARISH6281@users.noreply.github.com>
2023-07-05 10:04:10 +0530

    Add files via upload

---
+++ Alternative.java
@@ -0,0 +1,10 @@
+public class Alternative {
+    public static void main(String[] args) {
+        int[] arr ={1,2,3,4};
+        int n=4;
+        for(int i=0;i<n;i+=2){
+            System.out.print(arr[i]+" ");
+        }
+        
+    }
+}
